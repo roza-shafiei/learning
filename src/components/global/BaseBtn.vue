92bc412145cb5f1070ebd5262dcbd1863ee1fc6c
roza <roza_shafiei@yahoo.com>
2025-01-28 11:16:20 +0330

    feat: base button component

---
+++ src/components/global/BaseBtn.vue
@@ -0,0 +1,25 @@
+<script setup>
+const props = defineProps({
+  text: String
+})
+</script>
+
+<template>
+  <button class="base-btn" @click="$emit('click')">
+    {{ props.text }}
+  </button>
+</template>
+
+<style lang="scss">
+.base-btn {
+  display: inline-block;
+  background: var(--color-primary);
+  color: var(--color-white-100);
+  padding: toRem(12) toRem(24);
+  border: none;
+  outline: none;
+  border-radius: toRem(24);
+  font-weight: 600;
+  cursor: pointer;
+}
+</style>
