205426dec7363f35b13f65017abc6f26cc411783
roza <roza_shafiei@yahoo.com>
2025-02-01 12:23:24 +0330

    style: main app component layout structure

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,21 @@ import TheNavbar from '@/components/layouts/TheNavbar.vue'
 </script>
 
 <template>
-  <TheNavbar />
-  <RouterView />
-  <TheFooter />
+  <div class="main-layout">
+    <div>
+      <TheNavbar />
+      <RouterView />
+    </div>
+    <TheFooter />
+  </div>
 </template>
 
 <style scoped>
-
+.main-layout {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  height: 100%;
+  min-height: 100vh;
+}
 </style>
