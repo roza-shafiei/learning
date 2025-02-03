00b2f2166ba3eec945be2675193c727e6ec5d9e0
roza <roza_shafiei@yahoo.com>
2025-02-03 20:09:37 +0330

    feat: add faqlist to use in course id page

---
+++ src/components/pages/course/FaqList.vue
@@ -0,0 +1,51 @@
+<script setup>
+import faqList from '@/utils/faqList.js'
+import { ref } from 'vue'
+
+const activeFaq = ref(0)
+
+</script>
+
+<template>
+  <section class="faq">
+    <div v-for="(faq, index) in faqList" :key="index"
+         class="faq-item" @click="activeFaq = index">
+      <button class="accordion">{{ faq.title }}</button>
+      <div :class="activeFaq === index ? 'active':''" class="panel">
+        <p>{{ faq.desc }}</p>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+.accordion {
+  background-color: var(--color-white-100);
+  color: #444;
+  cursor: pointer;
+  padding: 18px;
+  width: 100%;
+  border: none;
+  text-align: left;
+  outline: none;
+  font-size: 15px;
+  transition: 0.4s;
+  overflow: hidden;
+}
+
+.active, .accordion:hover {
+  background-color: #ccc;
+}
+
+.panel {
+  display: none;
+  background-color: var(--color-white-200);
+  overflow: hidden;
+  padding: toRem(12) toRem(16);
+}
+
+.panel.active {
+  display: block;
+}
+
+</style>
---
+++ src/utils/faqList.js
@@ -0,0 +1,28 @@
+export default [
+  {
+    title: 'Who should take an Online Course?',
+    desc: 'Students join our online courses for a variety of reasons and come from diverse backgrounds, professions, and age groups. With online courses, you can\n' +
+      'Advance your skill-set or begin a career change.\n' +
+      'Take the next step in your formal education.\n' +
+      'Ease the transition before entering a full-time academic program.\n' +
+      'Enjoy learning a new topic.\n' +
+      'Whatever the reason, our online courses are a flexible way of learning about the art world without disrupting your daily life.'
+  },
+  {
+    title: 'How do I select a course?',
+    desc: 'Visit the course catalog to browse our offerings. In addition to searching by course duration' +
+      'Visit the course catalog to browse our offerings. In addition to searching by course duration and availability, you can also search by subject to ensure you find a course that reflects your interests. Though it is not required, we encourage those who have not studied art before to consider taking an art history course first to get a solid grounding in the discipline before exploring more specific disciplines.' +
+      'Our art history courses are the perfect first step before moving on to art market courses.' +
+      'and availability, you can also search by subject to ensure you find a course that reflects your interests. Though it is not required, we encourage those who have not studied art before to consider taking an art history course first to get a solid grounding in the discipline before exploring ' +
+      'more specific disciplines. Our art history courses are the perfect first step before moving on to art market courses.'
+  },
+  {
+    title: 'Are there prerequisites or language requirements?',
+    desc: 'There are no prerequisites. Our courses are open to students of all backgrounds who are 18 years of age and older.\n' +
+      'All courses are conducted in English. Video lectures include English subtitles and the option to slow-down, pause, or replay lectures for better retention. Discussion boards are a critical component of each of our courses; therefore, we suggest students have a conversational knowledge of English when pursuing a Certificate of Completion. For any of the art writing courses, students need to be 100% proficient in written English.'
+  },
+  {
+    title: 'I am a high school student, can I join?',
+    desc: 'Courses are open to all students 18 years or older. If you are under 18 years old, please contact us (roza_shafiei@yahoo.com) to discuss further.'
+  }
+]
