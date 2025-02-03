2ce7f10ade8984614fd7c1efac0304dc4fed722d
roza <roza_shafiei@yahoo.com>
2025-02-03 21:18:44 +0330

    fix: define formKit plugin in main.js file

--- src/main.js
+++ src/main.js
@@ -4,8 +4,12 @@ import router from './router/index.js'
 import { createApp } from 'vue'
 import App from './App.vue'
 import { pinia } from '@/store/index.js'
+import { defaultConfig, plugin } from '@formkit/vue'
+
 
 const app = createApp(App)
 app.use(router)
 app.use(pinia)
+app.use(plugin, defaultConfig)
+
 app.mount('#app')
