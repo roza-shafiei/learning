fd2f71bde8d008e555da796e1957a1192a4092be
roza <roza_shafiei@yahoo.com>
2025-02-08 13:21:43 +0330

    feat: add loading for base button

--- src/components/global/BaseBtn.vue
+++ src/components/global/BaseBtn.vue
@@ -1,17 +1,41 @@
 <script setup>
+
 const props = defineProps({
-  text: String
+  text: String,
+  loading: Boolean
 })
 </script>
 
 <template>
   <button class="btn base-btn" @click="$emit('click')">
-    {{ props.text }}
+    <span v-if="!loading"> {{ props.text }}</span>
+    <span v-else :style="loading ? 'display:block':'display:none'" class="btn-ring"
+    ></span>
   </button>
 </template>
 
 <style lang="scss">
 .btn {
   @include base-btn;
+}
+
+.btn-ring:after {
+  content: "";
+  display: block;
+  width: 16px;
+  height: 16px;
+  border-radius: 50%;
+  border: 1px solid;
+  border-color: var(--color-white-100) transparent var(--color-white-100) transparent;
+  animation: ring 1.2s linear infinite;
+}
+
+@keyframes ring {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
 }
 </style>
