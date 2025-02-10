06e2ac742b63ae24a47dc01881c9e68a3990b7f2
roza <roza_shafiei@yahoo.com>
2025-02-10 15:55:05 +0330

    fix: handle errors in teacher list fetch

--- src/views/teacher/TeacherListView.vue
+++ src/views/teacher/TeacherListView.vue
@@ -8,6 +8,7 @@ const loading = ref(false)
 const baseURL = import.meta.env.VITE_BASE_URL
 const teacherList = ref([])
 const activeTeacherId = ref(null)
+const errorMsg = ref('')
 
 // Lifecycle hook
 onMounted(() => {
@@ -23,9 +24,17 @@ async function getTeacherList() {
         'Content-Type': 'application/json'
       }
     })
-    teacherList.value = await res.json()
+    const resData = await res.json()
+    for (const key in resData) {
+      const teacherObj = { ...resData[key] }
+      teacherList.value.push(teacherObj)
+    }
   } catch (error) {
-    console.log('get teacher list error', error)
+    if (error.message.includes('fetch')) {
+      errorMsg.value = 'Please check your connection or VPN, then try again.'
+    } else {
+      errorMsg.value = 'Sorry, something went wrong!'
+    }
   } finally {
     loading.value = false
   }
@@ -44,14 +53,17 @@ function handleFormSubmit() {
   <main class="teachers-page max-content">
     <TheLoading v-if="loading" class="mx-auto" />
     <section v-else class="teachers">
-      <TeacherCard
-        v-for="teacher in teacherList"
-        :key="teacher.id"
-        :is-form-visible="activeTeacherId === teacher.id"
-        :item="teacher"
-        @contact-click="handleContactClick(teacher.id)"
-        @form-submit="handleFormSubmit"
-      />
+      <template v-if="teacherList.length > 0">
+        <TeacherCard
+          v-for="teacher in teacherList"
+          :key="teacher.id"
+          :is-form-visible="activeTeacherId === teacher.id"
+          :item="teacher"
+          @contact-click="handleContactClick(teacher.id)"
+          @form-submit="handleFormSubmit"
+        />
+      </template>
+      <p v-else class="error-msg">{{ errorMsg }}</p>
     </section>
   </main>
 </template>
@@ -72,6 +84,11 @@ function handleFormSubmit() {
     @include mq(lg) {
       max-width: 70%;
     }
+  }
+
+  .error-msg {
+    text-align: center;
+    font-size: toRem(14);
   }
 }
 </style>
